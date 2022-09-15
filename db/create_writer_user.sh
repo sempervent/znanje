891770b36fac9315c53f312e9d8bd272c0da66df
sempervent <jngrant@gmail.com>
2022-09-14 20:16:07 -0400

    add script to create writer user and znanje database

--- db/create_writer_user.sh
+++ db/create_writer_user.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 # create the writer user 
+set -e
 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER writer;
+  CREATE DATABASE znanje;
+  GRANT ALL PRIVILEGES ON DATABASE znanje TO writer;
+EOSQL
